6dc654f241bd317293ea7fe57b35b1b9cdf06049
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2024-03-08 19:09:07 +0530

    UI improvements

--- app/(landing)/_components/feature.tsx
+++ app/(landing)/_components/feature.tsx
@@ -1,27 +1,26 @@
 "use client";
 
+import { ArrowRight } from "lucide-react";
+
 export const Feature = () => {
 
     return (
-        <div className="lg:flex items-center justify-center max-w-7xl bg-[#51619A] rounded-[31px] p-10 mb-[1.5rem]">
+        <div className="lg:flex  max-w-7xl bg-[#51619A] rounded-[31px] mb-[8rem] gap-[7rem] pl-[7rem] pr-[7rem] pt-[4rem] pb-[4rem]">
             <div className="text-start font-sans text-white">
                 <h1 className="text-5xl">
-                    Meet <span className="font-bold"> Recruto <span className="text-[#E9357B]">AI</span> </span>
+                    Meet <span className="font-bold"> Recruto<span className="text-[#E9357B]">AI</span> </span>
                 </h1>
                 <h1 className="text-5xl">
                     Hiring on Auto-Pilot.
                 </h1>
-                <p className="text-[27px] mt-[40px] mb-[40px]">
+                <p className="w-[28rem] text-[24px] mt-[40px] mb-[40px]">
                     Recruto does all - Generate JD, Set custom screening, Shortlist Candidates,
                     Setup interviews — all in a matter of days.
                     It's that easy.
                 </p>
-                <button className="bg-[#0085FF] rounded-[7px] h-[40px] text-white pr-[20px] pl-[20px] text-[15px]"> Hire Talent -> </button>
+                <button className="flex items-center justify-center gap-2 bg-[#0085FF] rounded-[7px] h-[40px] text-white pr-[20px] pl-[20px] text-[15px]"> Hire Talent <ArrowRight size={"15px"} /> </button>
             </div>
-            <img className="lg:w-[30rem] sm:w-[2rem]" src="scroll-1.png" alt="" />
-            {/* <img className="lg:w-[30rem] sm:w-[2rem]" src="second-scroll.png" alt="" />
-            <img className="lg:w-[30rem] sm:w-[2rem]" src="third-scroll.png" alt="" /> */}
-
+            <img className="lg:w-[30rem] sm:w-[2rem]" src="total.png" alt="" />
         </div>
     )
 }
--- app/(landing)/_components/footer.tsx
+++ app/(landing)/_components/footer.tsx
@@ -3,18 +3,18 @@
 export const Footer = () => {
 
     return (
-        <div className="lg:flex border items-center justify-center max-w-7xl mb-[3rem]">
+        <div className="lg:flex border items-center justify-center max-w-7xl gap-10 mb-[3rem]">
             <img src="join.png" alt="" />
-            <div className="text-start font-sans ml-[4rem]">
+            <div className="text-start font-sans ">
                 <h1 className="text-5xl">
                     Get Started Today
                 </h1>
                 <p className="text-[27px] mt-[40px] mb-[40px]">
                     To apply to jobs with one-click and connect with founders and recruiters searching for your skills.
                 </p>
-                <div className="flex">
-                    <button className="bg-[#0085FF] rounded-[7px] h-[40px] text-white pr-[20px] pl-[20px] text-[15px]"> Explore Jobs </button>
-                    <button className="bg-[#0085FF] rounded-[7px] h-[40px] text-white pr-[20px] pl-[20px] text-[15px]"> Create your profile </button>
+                <div className="flex gap-5">
+                    <button className="bg-[#0085FF] rounded-[7px] h-[30px] text-white pr-[20px] pl-[20px] text-[14px]"> Explore Jobs </button>
+                    <button className="border rounded-[7px] h-[30px]  pr-[20px] pl-[20px] text-[14px]"> Create your profile </button>
                 </div>
             </div>
         </div>
