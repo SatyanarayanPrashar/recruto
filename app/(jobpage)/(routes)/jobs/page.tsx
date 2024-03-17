93c54b3fe6dc388022ed2d3a0768a43ef4b7f517
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2024-03-18 00:21:05 +0530

    fixed infinite scrolling

--- app/(jobpage)/(routes)/jobs/page.tsx
+++ app/(jobpage)/(routes)/jobs/page.tsx
@@ -1,24 +1,57 @@
 "use client"
 
-import { Profile } from "@/lib/profileobj";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { HeroSection } from "../../_components/hero";
 import { JobSection } from "../../_components/jobs";
 import { CircularProgress } from "@mui/material";
-import { auth } from "@/providers/auth-provider";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { Jobs } from "@/lib/jobsobj";
 
 const JobPage = () => {
+  // const [loading, setLoading] = useState(true);
+  const [jobContent, setJobContent] = useState<Jobs[]>([]);
+  const [page, setPage] = useState(1);
+  
+  useEffect(() => {
+    // fetchInitialContent(page);
+      const handleScroll = () => {
+        console.log("Scroll position:", document.documentElement.scrollTop);
+          if (
+              window.innerHeight + document.documentElement.scrollTop >= document.documentElement.scrollHeight / 1.2 
+              // !loading
+          ) {
+              setPage(prevPage => prevPage + 1);
+          }
+      };
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        console.log("Scroll event listener removed."); // Debugging line
+        window.removeEventListener('scroll', handleScroll);
+    };
+  }, [ page]);
 
+  useEffect(() => {
+    fetchInitialContent(page);
+  }, [page]);
+
+  const fetchInitialContent = async (page: number) => {
+      // setLoading(true);
+      let url = `http://127.0.0.1:8000/api/jobs/?page=${page}`;
+      try {
+          const response = await axios.get(url);
+          setJobContent(prevContent => [...prevContent, ...response.data.results]);
+          // setLoading(false);
+      } catch (error) {
+          console.error('Error fetching initial data:', error);
+          // setLoading(false);
+      }
+  };
+  
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <HeroSection />
-        
-          <JobSection/>
-      
+          <JobSection jobContent={jobContent}/>   
         <p>Designed and Built by Satya</p>
       </div>
     </div>
--- app/(jobpage)/_components/jobs.tsx
+++ app/(jobpage)/_components/jobs.tsx
@@ -1,61 +1,10 @@
 "use client";
 
 import { JobTile } from "@/components/JobTile";
-import { Jobs } from "@/lib/jobsobj";
-import { auth } from "@/providers/auth-provider";
-import { CircularProgress } from "@mui/material";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { Key } from "react";
 
-export const JobSection = () => {
-    const [loading, setLoading] = useState(true);
-    const [jobContent, setJobContent] = useState<Jobs[]>([]); // Initialize as an empty array instead of null
-    const [user] = useAuthState(auth);
-    const [page, setPage] = useState(1);
-    
-    useEffect(() => {
-        const handleScroll = () => {
-            if (
-                window.innerHeight + document.documentElement.scrollTop >= document.documentElement.scrollHeight / 1 &&
-                !loading
-            ) {
-                setPage(prevPage => prevPage + 1);
-            }
-        };
-    
-        window.addEventListener('scroll', handleScroll);
-        console.log('i am handle scroll fire once');
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, [loading]);
 
-    useEffect(() => {
-        // if (!selectedCountry && !selectedHeading) {
-            fetchInitialContent(page);
-        // } else {
-
-        //     fetchFilteredContent(selectedCountry, selectedHeading, page);
-        // }
-    }, [page]);
-
-    const fetchInitialContent = async (page: number) => {
-        setLoading(true);
-        const url = `http://127.0.0.1:8000/api/jobs/?page=${page}`;
-
-        try {
-            const response = await axios.get(url);
-            setJobContent(prevContent => [...prevContent, ...response.data.results]);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching initial data:', error);
-            setLoading(false);
-        }
-    };
-    
-
-    if (loading) {
-        return <div className="w-full flex justify-center"><CircularProgress size="25px" color="secondary" /></div>;
-    }
+export const JobSection = ({ jobContent }: { jobContent: any }) => {
 
     return (
         <div className="lg:flex sm:block items-start justify-start max-w-[75rem] mt-[-50px] mb-[8rem] gap-10 text-start">
@@ -64,7 +13,7 @@ export const JobSection = () => {
                     <h1 className="text-[1.3rem] font-[600]">   
                         Trending Jobs<span className="text-[#E9357B]">:</span>
                     </h1>
-                    {jobContent?.map((job, index) => (
+                    {jobContent?.map((job: { title: string | null; company: string | null; job_type: string | null; location: string | null; industry: string | null; remote_policy: string | null; }, index: Key | null | undefined) => (
                         <div key={index}>
                             <JobTile
                                 title={job.title}
